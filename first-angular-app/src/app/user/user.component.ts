ae8f84b2385719a60555e197b295001a5325d070
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-04-20 18:23:07 -0400

    refactor: remove unused user selection logic and UI elements
    - Cleaned up the user component by removing the image and name display.
    - Eliminated unused signals and computed properties for better clarity.

--- first-angular-app/src/app/user/user.component.ts
+++ first-angular-app/src/app/user/user.component.ts
@@ -1,7 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
-import { USERS } from './users';
-
-let randomIndex = Math.floor(Math.random() * USERS.length);
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -10,22 +7,6 @@ let randomIndex = Math.floor(Math.random() * USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  // signal to hold the selected user
-  // signal is a reactive primitive that allows you to create a reactive state
-  selectedUser = signal(USERS[randomIndex]);
-
-  // computed property to get the image path for the selected user
-  // computed is a reactive primitive that allows you to create a reactive property from another signal
-  imagePath = computed(() => 'users/' + this.selectedUser().avatar);
-
-  // get the image path for the selected user
-  // get imagePath() {
-  //   return `/users/${this.selectedUser.avatar}`;
-  // }
-
   // this method is called when the user clicks the button
-  onSelectUser() {
-    randomIndex = Math.floor(Math.random() * USERS.length);
-    this.selectedUser.set(USERS[randomIndex]);
-  }
+  onSelectUser() {}
 }
