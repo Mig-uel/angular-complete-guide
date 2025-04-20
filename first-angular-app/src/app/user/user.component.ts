6209b6d5408d19368be450cf872a2e9931b6f619
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-04-20 18:45:20 -0400

    feat: add user avatar and name inputs to UserComponent
    - Introduced @Input properties for avatar and name
    - Added imagePath getter to construct user image URL
    - Updated template to display user name and image

--- first-angular-app/src/app/user/user.component.ts
+++ first-angular-app/src/app/user/user.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import type { USERS } from './users';
 
 @Component({
   selector: 'app-user',
@@ -7,6 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
+  @Input({
+    required: true,
+  })
+  avatar!: (typeof USERS)[number]['avatar'];
+  @Input({
+    required: true,
+  })
+  name!: (typeof USERS)[number]['name'];
+
+  get imagePath() {
+    return `/users/${this.avatar}`;
+  }
+
   // this method is called when the user clicks the button
   onSelectUser() {}
 }
