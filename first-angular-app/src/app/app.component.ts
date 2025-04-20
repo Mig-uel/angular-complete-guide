c8c03c31dd856e5c2fafaf8262d0b20f6f51930a
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-04-20 17:05:29 -0400

    feat: add user component to app component template
    - Included <app-user /> in the app.component.html
    - Updated imports in app.component.ts to include UserComponent

--- first-angular-app/src/app/app.component.ts
+++ first-angular-app/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root', // this is the selector for the component, used in the HTML to render the component
   templateUrl: './app.component.html', // this is the template file for the component
   styleUrl: './app.component.css', // this is the CSS file for the component
-  imports: [HeaderComponent], // this is the list of components to import into the module
+  imports: [HeaderComponent, UserComponent], // this is the list of components to import into the module
 })
 export class AppComponent {}
