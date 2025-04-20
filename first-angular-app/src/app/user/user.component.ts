a9e4495070523130fff939105cb8a221b2078591
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-04-20 18:11:39 -0400

    fix: update user selection logic to use signals and computed
    - Refactor selectedUser to use signal for reactive state
    - Update imagePath to be a computed property for better reactivity
    - Ensure selected user updates correctly on button click

--- first-angular-app/src/app/user/user.component.ts
+++ first-angular-app/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { USERS } from './users';
 
 let randomIndex = Math.floor(Math.random() * USERS.length);
@@ -10,17 +10,22 @@ let randomIndex = Math.floor(Math.random() * USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  // this is a random user state from the USERS array
-  selectedUser = USERS[randomIndex];
+  // signal to hold the selected user
+  // signal is a reactive primitive that allows you to create a reactive state
+  selectedUser = signal(USERS[randomIndex]);
+
+  // computed property to get the image path for the selected user
+  // computed is a reactive primitive that allows you to create a reactive property from another signal
+  imagePath = computed(() => 'users/' + this.selectedUser().avatar);
 
   // get the image path for the selected user
-  get imagePath() {
-    return `/users/${this.selectedUser.avatar}`;
-  }
+  // get imagePath() {
+  //   return `/users/${this.selectedUser.avatar}`;
+  // }
 
   // this method is called when the user clicks the button
   onSelectUser() {
     randomIndex = Math.floor(Math.random() * USERS.length);
-    this.selectedUser = USERS[randomIndex];
+    this.selectedUser.set(USERS[randomIndex]);
   }
 }
