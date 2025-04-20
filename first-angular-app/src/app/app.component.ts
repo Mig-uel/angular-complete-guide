d62dbcc4b0e70aacbdff40ab7ccd690255a8f490
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-04-20 18:45:24 -0400

    feat: bind user data to UserComponent in app template
    - Updated app.component.html to bind user avatar and name
    - Introduced users array in app.component.ts for dynamic rendering

--- first-angular-app/src/app/app.component.ts
+++ first-angular-app/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { USERS } from './user/users';
 
 @Component({
   selector: 'app-root', // this is the selector for the component, used in the HTML to render the component
@@ -8,4 +9,6 @@ import { UserComponent } from './user/user.component';
   styleUrl: './app.component.css', // this is the CSS file for the component
   imports: [HeaderComponent, UserComponent], // this is the list of components to import into the module
 })
-export class AppComponent {}
+export class AppComponent {
+  users = USERS;
+}
