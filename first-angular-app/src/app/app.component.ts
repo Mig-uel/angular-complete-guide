35fa96f17e55ff0b5121dda5ab847c13d4ef0ec7
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-04-20 16:33:05 -0400

    feat: integrate HeaderComponent into app component
    - Added HeaderComponent to the app component imports
    - This allows the header to be rendered in the application

--- first-angular-app/src/app/app.component.ts
+++ first-angular-app/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { HeaderComponent } from './header.component';
   selector: 'app-root', // this is the selector for the component, used in the HTML to render the component
   templateUrl: './app.component.html', // this is the template file for the component
   styleUrl: './app.component.css', // this is the CSS file for the component
+  imports: [HeaderComponent], // this is the list of components to import into the module
 })
 export class AppComponent {
   title = 'first-angular-app';
