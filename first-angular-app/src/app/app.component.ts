e2b6f6af4fdae6d0c983e457874cea788cf58a82
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-04-20 20:23:27 -0400

    feat: add TasksComponent to AppComponent
    - Import TasksComponent in AppComponent
    - Update template to include TasksComponent

--- first-angular-app/src/app/app.component.ts
+++ first-angular-app/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { TasksComponent } from './tasks/tasks.component';
 import { UserComponent } from './user/user.component';
 import { USERS } from './user/users';
 
@@ -7,12 +8,15 @@ import { USERS } from './user/users';
   selector: 'app-root', // this is the selector for the component, used in the HTML to render the component
   templateUrl: './app.component.html', // this is the template file for the component
   styleUrl: './app.component.css', // this is the CSS file for the component
-  imports: [HeaderComponent, UserComponent], // this is the list of components to import into the module
+  imports: [HeaderComponent, UserComponent, TasksComponent], // this is the list of components to import into the module
 })
 export class AppComponent {
   users = USERS;
+  name!: string;
 
   clickedUser(id: string) {
-    console.log('Selected user with ID:', id);
+    const user = this.users.find((u) => u.id === id);
+
+    if (user) this.name = user.name;
   }
 }
