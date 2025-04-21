2638c22fd67d69b6a751717985182d45a88c42a2
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-04-21 14:53:17 -0400

    refactor: update template syntax and imports in AppComponent
    - Replace commented-out for loop with ngFor for user listing
    - Update conditional rendering to use ngIf and ng-template
    - Import NgFor and NgIf from Angular common module

--- first-angular-app/src/app/app.component.ts
+++ first-angular-app/src/app/app.component.ts
@@ -3,12 +3,13 @@ import { HeaderComponent } from './header/header.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { UserComponent } from './user/user.component';
 import { USERS } from './user/users';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root', // this is the selector for the component, used in the HTML to render the component
   templateUrl: './app.component.html', // this is the template file for the component
   styleUrl: './app.component.css', // this is the CSS file for the component
-  imports: [HeaderComponent, UserComponent, TasksComponent], // this is the list of components to import into the module
+  imports: [HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf], // this is the list of components to import into the module
 })
 export class AppComponent {
   users = USERS;
