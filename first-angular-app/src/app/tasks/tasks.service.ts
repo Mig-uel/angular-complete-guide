742acbab0bf1582f5fcc1e29f4102269eff2b06c
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-04-23 16:03:33 -0400

    refactor: initialize tasks array and load from localStorage
    - Removed hardcoded tasks and replaced with dynamic loading
    - Ensures tasks are persisted across sessions

--- first-angular-app/src/app/tasks/tasks.service.ts
+++ first-angular-app/src/app/tasks/tasks.service.ts
@@ -10,31 +10,13 @@ import type { Task } from '../task/task.model';
   providedIn: 'root',
 })
 export class TasksService {
-  private tasks = [
-    {
-      id: 't1',
-      userId: 'u1',
-      title: 'Master Angular',
-      summary:
-        'Learn all the basic and advanced features of Angular & how to apply them.',
-      dueDate: '2025-12-31',
-    },
-    {
-      id: 't2',
-      userId: 'u3',
-      title: 'Build first prototype',
-      summary: 'Build a first prototype of the online shop website',
-      dueDate: '2024-05-31',
-    },
-    {
-      id: 't3',
-      userId: 'u3',
-      title: 'Prepare issue template',
-      summary:
-        'Prepare and describe an issue template which will help with project management',
-      dueDate: '2024-06-15',
-    },
-  ];
+  private tasks: Task[] = [];
+
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+
+    if (tasks) this.tasks = JSON.parse(tasks);
+  }
 
   /**
    * Gets users tasks by userId
@@ -54,13 +36,22 @@ export class TasksService {
       ...task,
       userId,
     });
+    this.saveTasks();
   }
 
   /**
-   *
+   * Remove tasks by ID
    * @param {string} id - ID of the task to be removed
    */
   removeTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.saveTasks();
+  }
+
+  /**
+   *  Save tasks to localStorage
+   */
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
