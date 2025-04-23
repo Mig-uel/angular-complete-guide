eece60f8fb8e3a4b31eb6be867b600d62e9f6bc4
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-04-23 08:30:18 -0400

    feat: integrate new-task component and add task functionality
    - Added <app-new-task /> component to tasks template for task creation.
    - Implemented isAddingTask state to manage task addition.
    - Updated button to trigger task addition functionality.

--- first-angular-app/src/app/tasks/tasks.component.ts
+++ first-angular-app/src/app/tasks/tasks.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from '../task/task.component';
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @Component({
   selector: 'app-tasks',
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
@@ -12,6 +13,8 @@ export class TasksComponent {
   userId!: string;
   @Input()
   name!: string;
+
+  isAddingTask = false;
 
   tasks = [
     {
@@ -46,5 +49,9 @@ export class TasksComponent {
 
   onClickCompleteFromTaskComponent(taskId: string) {
     this.tasks = this.tasks.filter((task) => task.id !== taskId);
+  }
+
+  onAddTaskClicked() {
+    this.isAddingTask = true;
   }
 }
