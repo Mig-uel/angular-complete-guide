bda56421d60c3a5a484a618ebdfeea0378f8dfcf
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-04-23 17:15:12 -0400

    refactor: remove unused AppModule file
    - The AppModule file was empty and not utilized in the application.
    - This cleanup helps maintain a tidy codebase and improves clarity.

--- first-angular-app/src/app/app.module.ts
+++
@@ -1,7 +0,0 @@
-import { NgModule } from '@angular/core';
-
-@NgModule({
-  // declare and register all the components that need to work together (can also add directives)
-  declarations: [],
-})
-export class AppModule {}
