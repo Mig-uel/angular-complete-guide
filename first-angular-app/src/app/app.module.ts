b92ea9e74e086ebe18385557f35df3d6fbb73ece
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-04-23 17:30:36 -0400

    feat: add missing component imports to AppModule
    - Include HeaderComponent, UserComponent, and TasksComponent
      in the AppModule imports to ensure proper functionality.

--- first-angular-app/src/app/app.module.ts
+++ first-angular-app/src/app/app.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   // declare and register all the components that need to work together (can also add directives)
   declarations: [AppComponent],
+
+  // standalone components
+  imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent],
 
   // root component
   bootstrap: [AppComponent],
