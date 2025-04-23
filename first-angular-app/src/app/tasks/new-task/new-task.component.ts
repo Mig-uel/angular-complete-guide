78ec3cc522a267d670cdb180afdd39b8cbee9d5c
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-04-23 08:48:58 -0400

    feat: enhance new-task component with backdrop functionality
    - Added click event to backdrop for cancel action
    - Implemented EventEmitter to handle backdrop clicks

--- first-angular-app/src/app/tasks/new-task/new-task.component.ts
+++ first-angular-app/src/app/tasks/new-task/new-task.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-new-task',
   imports: [],
   templateUrl: './new-task.component.html',
-  styleUrl: './new-task.component.css'
+  styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
+  // custom event emitter for parent <Tasks />
+  @Output() isBackdropOrCancelClicked = new EventEmitter<void>();
 
+  // button click handler function passed to the button like react
+  onBackdropOrCancelClicked() {
+    this.isBackdropOrCancelClicked.emit();
+  }
 }
