4c5f25a00075d8155bd4ec74578505a94a15dde0
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-04-23 09:01:05 -0400

    feat: implement two-way data binding in new-task component
    - Added ngModel bindings for title, summary, and due date fields
    - Enables real-time data updates in the new-task form

--- first-angular-app/src/app/tasks/new-task/new-task.component.ts
+++ first-angular-app/src/app/tasks/new-task/new-task.component.ts
@@ -1,14 +1,20 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
   // custom event emitter for parent <Tasks />
   @Output() isBackdropOrCancelClicked = new EventEmitter<void>();
+
+  // old way two-way binding
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
 
   // button click handler function passed to the button like react
   onBackdropOrCancelClicked() {
