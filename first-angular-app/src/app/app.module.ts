42d291c60499b35d8802d7d01b629ff711a78409
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-04-23 17:12:21 -0400

    feat: add initial AppModule setup for Angular application
    - Create AppModule to serve as the root module
    - Prepare for future component declarations and registrations

---
+++ first-angular-app/src/app/app.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  // declare and register all the components that need to work together (can also add directives)
+  declarations: [],
+})
+export class AppModule {}
