e1f66d5633e692cff22c59986c552bae657dc8b5
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-04-23 09:21:26 -0400

    feat: add form submission handling to new-task component
    - Implement ngSubmit for form submission
    - Emit task data on form submission

--- first-angular-app/src/app/tasks/new-task/new-task.component.ts
+++ first-angular-app/src/app/tasks/new-task/new-task.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import type { Task } from '../../task/task.model';
 
 @Component({
   selector: 'app-new-task',
@@ -8,8 +9,11 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
-  // custom event emitter for parent <Tasks />
+  // custom event emitter for parent <Tasks /> to emit when user wants to close
   @Output() isBackdropOrCancelClicked = new EventEmitter<void>();
+
+  // custom even emitter for parent <Tasks /> to emit when form was submitted
+  @Output() add = new EventEmitter<Omit<Task, 'userId'>>();
 
   // old way two-way binding
   enteredTitle = '';
@@ -24,5 +28,15 @@ export class NewTaskComponent {
   // button click handler function passed to the button like react
   onBackdropOrCancelClicked() {
     this.isBackdropOrCancelClicked.emit();
+  }
+
+  // ngSubmit handler function
+  onFormSubmit() {
+    this.add.emit({
+      dueDate: this.enteredDate,
+      id: crypto.randomUUID(),
+      summary: this.enteredSummary,
+      title: this.enteredTitle,
+    });
   }
 }
