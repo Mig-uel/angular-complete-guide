9d0ddf5be6e6fce60ee7fb0a1a23b3fa790d0d3b
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-04-23 17:21:31 -0400

    refactor: remove unused component imports in AppComponent
    - Cleaned up the AppComponent by removing imports for
      HeaderComponent, UserComponent, and TasksComponent
      as they are no longer needed.

--- first-angular-app/src/app/app.component.ts
+++ first-angular-app/src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
-import { TasksComponent } from './tasks/tasks.component';
-import { UserComponent } from './user/user.component';
 import { USERS } from './user/users';
 
 @Component({
+  standalone: false,
   selector: 'app-root', // this is the selector for the component, used in the HTML to render the component
   templateUrl: './app.component.html', // this is the template file for the component
   styleUrl: './app.component.css', // this is the CSS file for the component
-  imports: [HeaderComponent, UserComponent, TasksComponent], // this is the list of components to import into the module
 })
 export class AppComponent {
   users = USERS;
