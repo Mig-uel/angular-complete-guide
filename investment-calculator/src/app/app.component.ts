81b3eb1cf58ee24126a30514070d7713b10bd420
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-04-24 16:39:37 -0400

    refactor: remove imports from AppComponent and use NgModule
    - Simplified AppComponent by removing unnecessary imports.
    - Moved component declarations to AppModule for better organization.

--- investment-calculator/src/app/app.component.ts
+++ investment-calculator/src/app/app.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
-import { InvestmentResultsComponent } from './investment-results/investment-results.component';
-import { UserInputComponent } from './user-input/user-input.component';
 
 @Component({
+  standalone: false,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
 })
 export class AppComponent {}
