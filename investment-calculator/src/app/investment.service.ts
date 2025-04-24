a50e051ec7001f54fd07b348ee04d72bb594fa5e
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-04-24 16:17:03 -0400

    feat: add investment service for calculating results
    - Introduces InvestmentService to handle investment calculations
    - Provides method to calculate annual investment results based on user input

---
+++ investment-calculator/src/app/investment.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import type { AnnualData, InvestmentInput } from './investment-input.model';
+
+// can generate a service using 'ng generate service <service-name>
+@Injectable({
+  providedIn: 'root', // singleton service auto-injected app-wide
+})
+export class InvestmentService {
+  resultsData?: AnnualData[];
+
+  calculateInvestmentResults(data: InvestmentInput) {
+    const annualData = [];
+    let investmentValue = data.initialInvestment;
+
+    for (let i = 0; i < data.duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear =
+        investmentValue * (data.expectedReturn / 100);
+      investmentValue += interestEarnedInYear + data.annualInvestment;
+      const totalInterest =
+        investmentValue - data.annualInvestment * year - data.initialInvestment;
+      annualData.push({
+        year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: data.annualInvestment,
+        totalInterest,
+        totalAmountInvested:
+          data.initialInvestment + data.annualInvestment * year,
+      });
+    }
+
+    this.resultsData = annualData;
+  }
+}
+
+/**
+ * providedIn
+ *
+ * this tells Angular where to register the service in its Dependency Injection
+ * system.
+ *
+ * (how scoped or available that service should be)
+ *
+ * root: default and most common, registers the service app-wide (singleton)
+ * once instance shared across the entire app
+ *
+ * platform: shares a single instance across multiple Angular applications
+ * running on the same page, used in advanced scenarios like micro-frontend or
+ * Angular elements
+ *
+ * any: creates a new instance of the service for each lazy-loaded module that
+ * injects it, useful when you want separate instances of a service per module
+ */
