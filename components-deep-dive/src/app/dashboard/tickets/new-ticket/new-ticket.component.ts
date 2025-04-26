cfafaff387b68b82597e0b50d9dae9f1b86ff321
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-04-26 10:39:48 -0400

    refactor: clean up control component usage in new ticket form
    - Removed unnecessary class attributes for consistency
    - Standardized import statements for better readability

--- components-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ components-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { ButtonComponent } from "../../../shared/button/button.component";
-import { ControlComponent } from "../../../shared/control/control.component";
+import { ButtonComponent } from '../../../shared/button/button.component';
+import { ControlComponent } from '../../../shared/control/control.component';
 
 @Component({
   selector: 'app-new-ticket',
   imports: [ButtonComponent, ControlComponent],
   templateUrl: './new-ticket.component.html',
-  styleUrl: './new-ticket.component.css'
+  styleUrl: './new-ticket.component.css',
 })
-export class NewTicketComponent {
-
-}
+export class NewTicketComponent {}
