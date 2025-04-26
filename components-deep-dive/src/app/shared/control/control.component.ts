c1fb8b2ecbef4953a9649f95a8343cc6a359c783
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-04-26 09:51:34 -0400

    feat: enhance control component with encapsulation and styles
    - Added ViewEncapsulation.None to the control component for better
      style scoping.
    - Updated HTML structure to include a class for styling.
    - Added documentation on Shadow DOM usage for clarity.

--- components-deep-dive/src/app/shared/control/control.component.ts
+++ components-deep-dive/src/app/shared/control/control.component.ts
@@ -1,11 +1,30 @@
-import { Component, input } from '@angular/core';
+import { Component, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   imports: [],
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None,
 })
 export class ControlComponent {
   label = input.required<string>();
 }
+
+/**
+ * The Shadow Dom
+ *
+ * A browser feature that allows you to attach hidden DOM structures to DOM
+ * elements
+ *
+ * Example:
+ * The built-in <video> element hides a more complex DOM tree that's
+ * used internally
+ *
+ * For CSS styling, the Shadow DOM can be used to scope CSS styles to
+ * that hidden tree - instead of applying styles globally to the
+ * entire page
+ *
+ * Angular can emulate this Shadow DOM browser feature for its own
+ * components
+ */
