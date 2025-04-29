6e73cbe71d94aafe582f010ca628a116693c31b2
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-04-29 13:20:10 -0400

    refactor: reorganize lifecycle hooks and improve comments
    - Rearranged import statements for better readability
    - Enhanced comments for lifecycle hooks to clarify their purpose

--- lifecycle-deep-dive-start/src/app/lifecycle/lifecycle.component.ts
+++ lifecycle-deep-dive-start/src/app/lifecycle/lifecycle.component.ts
@@ -1,22 +1,22 @@
 import {
-  Component,
-  Input,
-  OnInit,
-  OnChanges,
-  DoCheck,
-  AfterContentInit,
   AfterContentChecked,
-  AfterViewInit,
+  AfterContentInit,
   AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  Input,
+  OnChanges,
   OnDestroy,
+  OnInit,
   SimpleChanges,
 } from '@angular/core';
 
 @Component({
-    selector: 'app-lifecycle',
-    imports: [],
-    templateUrl: './lifecycle.component.html',
-    styleUrl: './lifecycle.component.css'
+  selector: 'app-lifecycle',
+  imports: [],
+  templateUrl: './lifecycle.component.html',
+  styleUrl: './lifecycle.component.css',
 })
 export class LifecycleComponent
   implements
@@ -44,27 +44,50 @@ export class LifecycleComponent
     console.log(changes);
   }
 
+  // invoked by angular whenever it thinks a UI update might be needed anywhere
   ngDoCheck() {
+    // discouraged to use this hook as it is system-wide
     console.log('ngDoCheck');
   }
 
+  // invoked when 'ng-content' content has been initialized
   ngAfterContentInit() {
+    // we could access that content and get information about it
     console.log('ngAfterContentInit');
   }
 
+  // invoked when the content has been detected by angular change
+  // detection mechanism
   ngAfterContentChecked() {
     console.log('ngAfterContentChecked');
   }
 
+  // invoked when the template is init/rendered
   ngAfterViewInit() {
     console.log('ngAfterViewInit');
   }
 
+  // invoked when angular performs change detection for this component
+  // view/template
   ngAfterViewChecked() {
     console.log('ngAfterViewChecked');
   }
 
+  // invoked when this component is about to be thrown away aka destroyed
   ngOnDestroy() {
     console.log('ngOnDestroy');
   }
 }
+
+/**
+ * Technically, the 'View' is an internally managed data structure
+ * that hold references to the DOM elements rendered by a
+ * component
+ */
+
+/**
+ * Content might be any content that might be 'projected' in a view
+ * (done with ng-content)
+ * So the 'Content' is some other (partial) View data structure
+ * projected into this component's view
+ */
