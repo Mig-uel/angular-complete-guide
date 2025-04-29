6ad87152d63adae7f38ae36d893d858e6c8df190
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-04-29 17:45:18 -0400

    feat: implement dynamic sizing for rectangle component
    - Update rectangle dimensions to use dynamic input properties
    - Enhance two-way binding functionality for better user interaction

--- custom-2way-binding/src/app/rect/rect.component.ts
+++ custom-2way-binding/src/app/rect/rect.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
-    selector: 'app-rect',
-    imports: [],
-    templateUrl: './rect.component.html',
-    styleUrl: './rect.component.css'
+  selector: 'app-rect',
+  imports: [],
+  templateUrl: './rect.component.html',
+  styleUrl: './rect.component.css',
 })
 export class RectComponent {
-  // Todo: Implement custom two-way binding
+  // two way binding is the combination of input and output
+  // we are accepting a value but also updating a value
+  @Input({ required: true }) size!: { width: string; height: string };
+  @Output() sizeChange = new EventEmitter<{ width: string; height: string }>();
 
   onReset() {
-    // ...
+    this.sizeChange.emit({
+      height: '100',
+      width: '100',
+    });
   }
 }
