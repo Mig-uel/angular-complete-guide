bda4f69ee9efe170a921e44e702a50c9f58bce6e
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-04-29 15:00:55 -0400

    refactor: implement AfterViewInit for form handling
    - Updated NewTicketComponent to implement AfterViewInit
    - Ensures guaranteed access to form elements after initialization

--- components-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ components-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,10 @@
-import { Component, viewChild, type ElementRef } from '@angular/core';
+import {
+  Component,
+  viewChild,
+  type AfterViewInit,
+  type ElementRef,
+  type OnInit,
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
@@ -9,11 +15,18 @@ import { ControlComponent } from '../../../shared/control/control.component';
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
 })
-export class NewTicketComponent {
+export class NewTicketComponent implements AfterViewInit {
   // @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
   private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
 
   onFormSubmit(title: string, ticketText: string) {
     this.form().nativeElement.reset();
+  }
+
+  ngAfterViewInit(): void {
+    // guaranteed access to elements that have been selected by
+    // viewChild decorator, and that the template has been init
+    console.log('AFTER VIEW INIT');
+    console.log(this.form().nativeElement);
   }
 }
