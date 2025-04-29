a33a7345edcbd75e2c117d06be7bad2895895cff
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-04-29 15:38:45 -0400

    feat: add input handling for TicketComponent data
    - Introduced input property to TicketComponent for required Ticket data.
    - Enhances component functionality for better data management.

--- components-deep-dive/src/app/dashboard/tickets/ticket/ticket.component.ts
+++ components-deep-dive/src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import type { Ticket } from '../ticket.model';
 
 @Component({
   selector: 'app-ticket',
   imports: [],
   templateUrl: './ticket.component.html',
-  styleUrl: './ticket.component.css'
+  styleUrl: './ticket.component.css',
 })
 export class TicketComponent {
-
+  data = input.required<Ticket>();
 }
