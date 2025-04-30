fa6b74e6e91fa294aca10eae84fe49bdf95e09c6
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-04-30 15:11:11 -0400

    docs: update app.component.html and app.component.ts
    - Clarify the need to import NgIf for using *ngIf directive
    - Enhance understanding of conditional rendering in Angular

--- directives-deep-dive/src/app/app.component.ts
+++ directives-deep-dive/src/app/app.component.ts
@@ -1,16 +1,15 @@
-import { Component, computed, signal, type Signal } from '@angular/core';
+import { Component, computed, type Signal } from '@angular/core';
 
 import { AuthComponent } from './auth/auth.component';
-import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
 import { AuthService } from './auth/auth.service';
-import { NgIf } from '@angular/common';
+import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [AuthComponent, LearningResourcesComponent, NgIf],
+  imports: [AuthComponent, LearningResourcesComponent],
 })
 export class AppComponent {
   isAdmin: Signal<boolean>;
