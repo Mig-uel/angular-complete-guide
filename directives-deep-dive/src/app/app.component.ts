9dbb60a0f96fb6ad1672a3a831071abe4257d9af
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-04-30 15:06:16 -0400

    feat: implement admin conditional rendering in app component
    - Added conditional rendering for admin users using @if syntax.
    - Updated imports to include NgIf and added isAdmin signal for permission checks.

--- directives-deep-dive/src/app/app.component.ts
+++ directives-deep-dive/src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal, type Signal } from '@angular/core';
 
 import { AuthComponent } from './auth/auth.component';
 import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
+import { AuthService } from './auth/auth.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [AuthComponent, LearningResourcesComponent],
+  imports: [AuthComponent, LearningResourcesComponent, NgIf],
 })
-export class AppComponent {}
+export class AppComponent {
+  isAdmin: Signal<boolean>;
+
+  constructor(private authService: AuthService) {
+    this.isAdmin = computed(() => authService.activePermission() === 'admin');
+  }
+}
