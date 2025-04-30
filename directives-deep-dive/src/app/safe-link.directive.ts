757ab09a2be2c0229fee66d0a44f63b49bfc886d
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-04-30 15:31:52 -0400

    feat: implement SafeLinkDirective for link confirmation
    - Adds a custom directive to prompt users before leaving the app
    - Enhances user experience by preventing accidental navigation

--- directives-deep-dive/src/app/safe-link.directive.ts
+++ directives-deep-dive/src/app/safe-link.directive.ts
@@ -4,9 +4,21 @@ import { Directive } from '@angular/core';
 @Directive({
   selector: 'a[appSafeLink]',
   standalone: true,
+
+  host: {
+    '(click)': 'onConfirmLeavePage($event)',
+  },
 })
 export class SafeLinkDirective {
   constructor() {
     console.log('SafeLinkDirective is active');
+  }
+
+  onConfirmLeavePage(event: MouseEvent) {
+    const confirm = window.confirm('Do you want to leave the app?');
+
+    if (confirm) return;
+
+    event.preventDefault();
   }
 }
