3a96ce1912a6619adfdab8060a7f931b1949b4d4
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-05-02 00:34:01 -0400

    fix: enhance TemperaturePipe for configurable input/output types
    - Update transform method to accept input and output types
    - Ensure proper temperature conversion and symbol display

--- pipes-deep-dive/src/app/temperature.pipe.ts
+++ pipes-deep-dive/src/app/temperature.pipe.ts
@@ -10,15 +10,39 @@ export class TemperaturePipe implements PipeTransform {
    * the value after you use the pipe will be passed as an argument
    * to the transform method
    */
-  transform(value: number | string, ...args: any[]) {
+  transform(
+    value: number | string | null,
+    inputType: 'cel' | 'fah',
+    outputType?: 'cel' | 'fah'
+  ) {
+    // make the pipe configurable by accepting args
+
+    if (!value) return value;
+
     let val: number;
 
     if (typeof value === 'string') val = parseFloat(value);
     else val = value;
 
-    // celsius to fahrenheit
-    const celsiusTemp = val * (9 / 5) + 32;
+    let outputTemp: number;
 
-    return `${celsiusTemp} °C`;
+    // calc temperature conversion
+    if (inputType === 'cel' && outputType === 'fah')
+      outputTemp = val * (9 / 5) + 32;
+    else if (inputType === 'fah' && outputType === 'cel')
+      outputTemp = (val - 32) * (5 / 9);
+    else {
+      outputTemp = val;
+    }
+
+    let symbol: '°C' | '°F';
+
+    if (!outputType) {
+      symbol = inputType === 'cel' ? '°C' : '°F';
+    } else {
+      symbol = outputType === 'cel' ? '°C' : '°F';
+    }
+
+    return `${outputTemp.toFixed(2)} ${symbol}`;
   }
 }
