32d74954c6f1a5d0f6d3ef2446b3ed347b4b129e
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-03 16:30:25 -0400

    feat: implement lifecycle hooks in AvailablePlacesComponent
    - Added OnInit and OnDestroy interfaces for better lifecycle management
    - Subscribed to HTTP request for fetching available places
    - Unsubscribed from the observable to prevent memory leaks

--- placepicker/src/app/places/available-places/available-places.component.ts
+++ placepicker/src/app/places/available-places/available-places.component.ts
@@ -1,9 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, type OnDestroy, type OnInit } from '@angular/core';
 
-import { Place } from '../place.model';
-import { PlacesComponent } from '../places.component';
-import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
+import type { Subscription } from 'rxjs';
+import { Place } from '../place.model';
+import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { PlacesComponent } from '../places.component';
 
 @Component({
   selector: 'app-available-places',
@@ -12,8 +13,26 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit, OnDestroy {
   places = signal<Place[] | undefined>(undefined);
+  placesSubscription: Subscription | undefined = undefined;
 
   constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.placesSubscription = this.httpClient
+      .get<{ places: Place[] }>('http://localhost:3000/places', {
+        // observe: 'response',
+        // observe: 'events',
+      })
+      .subscribe({
+        next: (res) => {
+          console.log(res.places);
+        },
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.placesSubscription?.unsubscribe();
+  }
 }
