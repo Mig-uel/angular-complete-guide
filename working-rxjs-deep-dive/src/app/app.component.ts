4a01fdbf9b9c8589637d97f198d5f2b0282c1814
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-03 13:27:02 -0400

    feat: add RxJS map operator to interval subscription
    - Enhance the ngOnInit method to transform emitted values
      using the map operator for better data handling.

--- working-rxjs-deep-dive/src/app/app.component.ts
+++ working-rxjs-deep-dive/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, type OnDestroy, type OnInit } from '@angular/core';
-import { interval, type Subscription } from 'rxjs';
+import { interval, map, type Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,9 +10,13 @@ export class AppComponent implements OnInit, OnDestroy {
   private sub: Subscription | undefined = undefined;
 
   ngOnInit(): void {
-    this.sub = interval(1000).subscribe({
-      next: (val) => console.log(val),
-    });
+    this.sub = interval(1000)
+      .pipe(map((val) => val * 2))
+      // we can add multiple pipe operator which alter
+      // the emitted value
+      .subscribe({
+        next: (val) => console.log(val),
+      });
     // if you only care about the next case, you could also
     // just pass a function to subscribe instead of an obj
   }
