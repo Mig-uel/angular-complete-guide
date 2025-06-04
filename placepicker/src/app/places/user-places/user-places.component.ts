650343a5dd102da1d66945d31e57cc33f6580a03
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-04 12:14:25 -0400

    refactor: replace HttpClient with PlacesService for user places
    - Streamlined data fetching by using PlacesService
    - Improved code maintainability and readability

--- placepicker/src/app/places/user-places/user-places.component.ts
+++ placepicker/src/app/places/user-places/user-places.component.ts
@@ -1,10 +1,9 @@
 import { Component, signal, type OnDestroy, type OnInit } from '@angular/core';
-
-import { HttpClient } from '@angular/common/http';
-import { catchError, map, throwError, type Subscription } from 'rxjs';
+import { type Subscription } from 'rxjs';
 import { Place } from '../place.model';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
+import { PlacesService } from '../places.service';
 
 @Component({
   selector: 'app-user-places',
@@ -16,27 +15,16 @@ import { PlacesComponent } from '../places.component';
 export class UserPlacesComponent implements OnInit, OnDestroy {
   isFetching = signal(false);
   places = signal<Place[] | undefined>(undefined);
-  subscription: Subscription | undefined = undefined;
+  userPlacesSubscription: Subscription | undefined = undefined;
   error = signal('');
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private placesService: PlacesService) {}
 
   ngOnInit(): void {
     this.isFetching.set(true);
 
-    this.subscription = this.httpClient
-      .get<{ places: Place[] }>('http://localhost:3000/user-places')
-      .pipe(
-        map((res) => res.places),
-        catchError(() =>
-          throwError(
-            () =>
-              new Error(
-                'Something went wrong fetching your favorite places. Please try again later.'
-              )
-          )
-        )
-      )
+    this.userPlacesSubscription = this.placesService
+      .loadUserPlaces()
       .subscribe({
         next: (places) => {
           this.places.set(places);
@@ -50,6 +38,6 @@ export class UserPlacesComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-    this.subscription?.unsubscribe();
+    this.userPlacesSubscription?.unsubscribe();
   }
 }
