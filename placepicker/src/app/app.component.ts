2c611bfd565820b80a3b66ae968adf43e981bbeb
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-04 12:53:00 -0400

    feat: add error handling in app component
    - Integrate error handling by adding an error modal component.
    - Ensure users are notified of errors through the UI.

--- placepicker/src/app/app.component.ts
+++ placepicker/src/app/app.component.ts
@@ -2,12 +2,20 @@ import { Component } from '@angular/core';
 
 import { AvailablePlacesComponent } from './places/available-places/available-places.component';
 import { UserPlacesComponent } from './places/user-places/user-places.component';
+import { ErrorService } from './shared/error.service';
+import { ErrorModalComponent } from './shared/modal/error-modal/error-modal.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [AvailablePlacesComponent, UserPlacesComponent],
+  imports: [AvailablePlacesComponent, UserPlacesComponent, ErrorModalComponent],
 })
-export class AppComponent {}
+export class AppComponent {
+  error;
+
+  constructor(private errorService: ErrorService) {
+    this.error = errorService.error;
+  }
+}
