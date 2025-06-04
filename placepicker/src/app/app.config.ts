393a10751c6dd43a6eb94b6b221fb1f5b402793a
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-04 16:14:58 -0400

    feat: add logging interceptor for outgoing HTTP requests
    - Implemented a logging interceptor to log outgoing requests
    - Added a debug header for testing purposes

--- placepicker/src/app/app.config.ts
+++ placepicker/src/app/app.config.ts
@@ -1,9 +1,30 @@
-import { provideHttpClient } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptors,
+  type HttpHandlerFn,
+  type HttpRequest,
+} from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+
+function loggingInterceptor(
+  request: HttpRequest<unknown>,
+  next: HttpHandlerFn
+) {
+  const req = request.clone({
+    headers: request.headers.set('X-DEBUG', 'TESTING'),
+  });
+  console.log('[Outgoing Request]');
+  console.log(request);
+
+  // return next(req);
+  return next(request);
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideHttpClient(),
+
+    // http interceptors
+    provideHttpClient(withInterceptors([loggingInterceptor])),
   ],
 };
