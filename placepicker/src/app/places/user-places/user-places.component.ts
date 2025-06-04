5e3b4dafcbb0817f63d819972860daeca78a1520
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-04 11:44:06 -0400

    feat: implement user places fetching and state management
    - Add signals for loading state, fetched places, and error handling
    - Implement HTTP request to fetch user places on component initialization
    - Manage subscription and cleanup on component destruction

--- placepicker/src/app/places/user-places/user-places.component.ts
+++ placepicker/src/app/places/user-places/user-places.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, signal, type OnDestroy, type OnInit } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, throwError, type Subscription } from 'rxjs';
+import { Place } from '../place.model';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
 
@@ -10,5 +13,43 @@ import { PlacesComponent } from '../places.component';
   styleUrl: './user-places.component.css',
   imports: [PlacesContainerComponent, PlacesComponent],
 })
-export class UserPlacesComponent {
+export class UserPlacesComponent implements OnInit, OnDestroy {
+  isFetching = signal(false);
+  places = signal<Place[] | undefined>(undefined);
+  subscription: Subscription | undefined = undefined;
+  error = signal('');
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.isFetching.set(true);
+
+    this.subscription = this.httpClient
+      .get<{ places: Place[] }>('http://localhost:3000/user-places')
+      .pipe(
+        map((res) => res.places),
+        catchError(() =>
+          throwError(
+            () =>
+              new Error(
+                'Something went wrong fetching your favorite places. Please try again later.'
+              )
+          )
+        )
+      )
+      .subscribe({
+        next: (places) => {
+          this.places.set(places);
+          this.isFetching.set(false);
+        },
+        error: (e) => {
+          this.error.set(e);
+          this.isFetching.set(false);
+        },
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
 }
