cfa8a3bef2a68708f632902469d038e9b73fef84
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-07 11:24:05 -0400

    feat: migrate login form to reactive forms approach
    - Transitioned from template-driven to reactive forms for better control
    - Simplified form handling and validation
    - Removed unused code related to local storage and subscriptions

--- working-forms/src/app/auth/login/login.component.ts
+++ working-forms/src/app/auth/login/login.component.ts
@@ -1,62 +1,19 @@
-import {
-  afterNextRender,
-  Component,
-  signal,
-  viewChild,
-  type OnDestroy,
-} from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
-import { debounceTime, type Subscription } from 'rxjs';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
 })
-export class LoginComponent implements OnDestroy {
-  private form = viewChild.required<NgForm>('form');
-  private formSubscription: Subscription | undefined;
+export class LoginComponent {
+  // with the reactive approach we create the form group ourselves
+  form = new FormGroup({
+    email: new FormControl(),
+    password: new FormControl(),
+  });
 
-  constructor() {
-    afterNextRender(() => {
-      const savedEmail = window.localStorage.getItem('saved-email');
-
-      if (savedEmail) {
-        const loadedEmail = JSON.parse(savedEmail).email;
-
-        setTimeout(
-          () => this.form().controls['email'].setValue(loadedEmail),
-          1
-        );
-      }
-
-      this.formSubscription = this.form()
-        .valueChanges?.pipe(debounceTime(500)) // rxjs also provides a debounce pipe operator
-        .subscribe({
-          next: (value) =>
-            window.localStorage.setItem(
-              'saved-email',
-              JSON.stringify({ email: value.email })
-            ),
-        });
-    });
-  }
-
-  onSubmit(formData: NgForm) {
-    if (formData.form.invalid) return;
-
-    const { email, password } = formData.form.value;
-
-    console.log(formData.form);
-
-    // reset form on submission and any internal information
-    // like dirty, valid
-    formData.reset();
-  }
-
-  ngOnDestroy(): void {
-    this.formSubscription?.unsubscribe();
-  }
+  onSubmit() {}
 }
