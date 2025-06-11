725f83a3e43a3431842b7b0cca88b45cfae4b43b
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-11 09:29:26 -0400

    feat: enhance router configuration with input binding
    - Updated the router provider to include component input binding
    - Improves flexibility in handling component inputs during routing

--- routing-deep-dive/src/app/app.config.ts
+++ routing-deep-dive/src/app/app.config.ts
@@ -1,7 +1,7 @@
 import type { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)],
+  providers: [provideRouter(routes, withComponentInputBinding())],
 };
