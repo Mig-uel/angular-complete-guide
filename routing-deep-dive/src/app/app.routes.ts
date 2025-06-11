0b933ea5a8d2777120e3674ce5c51fd0a33c1f0c
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-11 09:41:23 -0400

    feat: add task routing under user tasks component
    - Introduced routing for tasks and new task creation
    - Enhances navigation for user-specific task management

--- routing-deep-dive/src/app/app.routes.ts
+++ routing-deep-dive/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import type { Routes } from '@angular/router';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +12,16 @@ export const routes: Routes = [
   {
     path: 'users/:uid', // <your-domain>/users/<uid>
     component: UserTasksComponent,
+
+    children: [
+      {
+        path: 'tasks', // <your-domain>/users/<uid>/tasks
+        component: TasksComponent,
+      },
+      {
+        path: 'tasks/new',
+        component: NewTaskComponent,
+      },
+    ],
   },
 ];
