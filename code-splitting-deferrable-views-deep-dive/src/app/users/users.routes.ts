0398dc70b7b2d196237a5326cff0bc591e327da3
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-12 17:03:46 -0400

    feat: Implement core application structure with routing and user task management
    - Added main application component with header and user list.
    - Created user and task components with respective styles and templates.
    - Implemented routing for user tasks and task creation.
    - Introduced services for managing users and tasks with local storage support.
    - Added responsive styles for better UI on different screen sizes.
    - Included dummy user data for initial testing and development.

---
+++ code-splitting-deferrable-views-deep-dive/src/app/users/users.routes.ts
@@ -0,0 +1,53 @@
+import { Routes, type ResolveFn } from '@angular/router';
+
+import {
+  NewTaskComponent,
+  canLeaveEditPage,
+} from '../tasks/new-task/new-task.component';
+import type { Task } from '../tasks/task/task.model';
+import { TasksService } from '../tasks/tasks.service';
+import { inject } from '@angular/core';
+
+const resolveUserTasks: ResolveFn<Task[]> = (activatedRouteSnapshot, _) => {
+  const order = activatedRouteSnapshot.queryParams['order'];
+  const tasksService = inject(TasksService);
+  const tasks = tasksService
+    .allTasks()
+    .filter(
+      (task) => task.userId === activatedRouteSnapshot.paramMap.get('userId')
+    );
+
+  if (order && order === 'asc') {
+    tasks.sort((a, b) => (a.id > b.id ? 1 : -1));
+  } else {
+    tasks.sort((a, b) => (a.id > b.id ? -1 : 1));
+  }
+
+  return tasks.length ? tasks : [];
+};
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'tasks',
+    pathMatch: 'full',
+  },
+  {
+    path: 'tasks', // <your-domain>/users/<uid>/tasks
+
+    // This is a lazy-loaded route, which means the component will be loaded only when the route is activated.
+    loadComponent: () =>
+      import('../tasks/tasks.component').then(
+        (module) => module.TasksComponent
+      ),
+    runGuardsAndResolvers: 'always',
+    resolve: {
+      userTasks: resolveUserTasks,
+    },
+  },
+  {
+    path: 'tasks/new',
+    component: NewTaskComponent,
+    canDeactivate: [canLeaveEditPage],
+  },
+];
