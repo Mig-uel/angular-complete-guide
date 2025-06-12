0454bbdf373f61e86744f6820a330394355c0771
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-12 16:01:08 -0400

    feat: add canDeactivate guard for unsaved changes warning
    - Implemented a navigation guard to prompt users
      about unsaved changes when attempting to leave the
      new task creation page.

--- routing-deep-dive/src/app/tasks/new-task/new-task.component.ts
+++ routing-deep-dive/src/app/tasks/new-task/new-task.component.ts
@@ -1,7 +1,7 @@
 import { Component, input, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { RouterLink, Router } from '@angular/router';
+import { Router, RouterLink, type CanDeactivateFn } from '@angular/router';
 import { TasksService } from '../tasks.service';
 
 @Component({
@@ -32,3 +32,23 @@ export class NewTaskComponent {
     this.router.navigate(['/users', this.uid(), 'tasks'], { replaceUrl: true });
   }
 }
+
+/**
+ * canDeactivate is a guard that can be used to prevent navigation
+ * CanDeactivateFn is a function that returns a boolean value or an observable that emits a boolean value, it is also a generic type that takes a component type as a parameter
+ */
+export const canLeaveEditPage: CanDeactivateFn<NewTaskComponent> = (
+  component
+) => {
+  if (
+    component.enteredTitle() ||
+    component.enteredSummary() ||
+    component.enteredDate()
+  ) {
+    return confirm(
+      'You have unsaved changes. Do you really want to leave this page?'
+    );
+  }
+
+  return true; // allow navigation if there are no unsaved changes
+};
