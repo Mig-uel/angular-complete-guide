2887bb6b85e2c6fe6d1dd79eec4c518a224f19fe
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-12 15:55:21 -0400

    feat: add route guard for user access control
    - Implemented a canMatch guard to control access to user routes
    - Added dummy logic for demonstration purposes
    - Redirects to home if access is denied

--- routing-deep-dive/src/app/app.routes.ts
+++ routing-deep-dive/src/app/app.routes.ts
@@ -1,4 +1,10 @@
-import type { Routes } from '@angular/router';
+import { inject } from '@angular/core';
+import {
+  RedirectCommand,
+  Router,
+  type CanMatchFn,
+  type Routes,
+} from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import {
@@ -7,6 +13,22 @@ import {
   UserTasksComponent,
 } from './users/user-tasks/user-tasks.component';
 import { routes as userRoutes } from './users/user.routes';
+
+const dummyCanMatch: CanMatchFn = (route, segments) => {
+  // we can inject any service here
+  // and any logic can be implemented
+  // and return true or false or an observable that emits a boolean value
+
+  const router = inject(Router);
+
+  const shouldGetAccess = Math.random(); // dummy logic for demonstration
+
+  if (shouldGetAccess < 0.5) {
+    return true;
+  }
+
+  return new RedirectCommand(router.parseUrl('/')); // redirect to home if access is denied
+};
 
 export const routes: Routes = [
   {
@@ -19,6 +41,22 @@ export const routes: Routes = [
     component: UserTasksComponent,
 
     children: userRoutes,
+
+    /**
+     * Route guards are functions that can be used to
+     * control access to routes based on certain conditions
+     * such as user authentication, authorization, etc.
+     * they can be used to prevent access to certain routes
+     * or to redirect users to other routes based on certain conditions.
+     * they are defined as functions that return a boolean value
+     * or an observable that emits a boolean value.
+     */
+
+    canMatch: [dummyCanMatch], // the most versatile guard that can be used to control access to the route
+
+    canActivate: [], // guard that can be used to control access to the route after it has been matched but before the component is loaded
+
+    canActivateChild: [], // guard that can be used to control access to child routes of the route
 
     // we can also define a data property on the route
     // this data can be used in the component
