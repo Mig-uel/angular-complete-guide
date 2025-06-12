7f72c95d3818ef505cf3c722e5d2cc73a4492fa0
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-12 14:02:46 -0400

    feat: implement OnInit interface in UserTasksComponent
    - Added OnInit interface to UserTasksComponent for lifecycle management
    - Introduced ngOnInit method to log the message input on initialization

--- routing-deep-dive/src/app/users/user-tasks/user-tasks.component.ts
+++ routing-deep-dive/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, computed, input, type OnInit } from '@angular/core';
 import { UsersService } from '../users.service';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
@@ -9,12 +9,24 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   styleUrl: './user-tasks.component.css',
   imports: [RouterOutlet, RouterLink],
 })
-export class UserTasksComponent {
+export class UserTasksComponent implements OnInit {
   uid = input.required<string>();
+  message = input<string>(''); // optional input, default is empty string
 
   constructor(private usersService: UsersService) {}
 
   userName = computed(
     () => this.usersService.users.find((u) => u.id === this.uid())?.name
   );
+
+  ngOnInit() {
+    // read the data passed to the route
+    console.log(this.message());
+  }
 }
+
+// we can get a hold of the data passed to the route in the component
+// by injecting the ActivatedRoute service
+// and accessing the `data` property
+// or if passed withComponentInputBinding we can use the
+// `@Input` decorator or `input` function to bind the data directly
